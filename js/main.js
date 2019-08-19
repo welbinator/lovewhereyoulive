ff3bd7f1ec15dc9b882faf83d72c2d1f0d403964
welbinator <james.welbes@gmail.com>
2019-08-18 22:26:47 -0500

    please still work

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 $(document).ready(function() {
     "use strict";
 
+     //------- smooth scrolling ------//
+
+
+     
+     // Scroll to specific values
+// scrollTo is the same
+window.scroll({
+    top: 2500, 
+    left: 0, 
+    behavior: 'smooth'
+  });
+  
+  // Scroll certain amounts from current position 
+  window.scrollBy({ 
+    top: 100, // could be negative value
+    left: 0, 
+    behavior: 'smooth' 
+  });
+  
+  // Scroll to a certain element
+  document.querySelector('.hello').scrollIntoView({ 
+    behavior: 'smooth' 
+  });
+
+
+
 
     //------- Niceselect  js --------//  
 
